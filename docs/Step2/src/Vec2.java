26767a35b40b51989f8589c3117908484115ca13
Klevis Pashollari <kpashollari@ikub.local>
2020-08-26 17:00:20 +0200

    add resource folders

---
+++ docs/Step2/src/Vec2.java
@@ -0,0 +1,30 @@
+public class Vec2 {
+		public float x;
+		public float y;
+
+		public Vec2(float x, float y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public Vec2 add(Vec2 b) {
+			return new Vec2(x + b.x, y + b.y);
+		}
+
+		public Vec2 sub(Vec2 b) {
+			return new Vec2(x - b.x, y - b.y);
+		}
+
+		public Vec2 mult(Vec2 b) {
+		return new Vec2(x * b.x, y * b.y);
+	}
+
+		public Vec2 mult(float m) {
+		return new Vec2(x * m, y * m);
+	}
+
+		public Vec2 div(Vec2 b) {
+			return new Vec2(x / b.x, y / b.y);
+		}
+
+	};
