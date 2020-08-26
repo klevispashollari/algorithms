0e8bb4832a3218f56c8f5e68922a6bb8d79bff60
gledis.gjinaj <gledis.gjinaj@moveoalbania.com>
2020-08-26 12:31:27 +0200

    Add keyAdapter for platformer

--- src/game/Platformer.java
+++ src/game/Platformer.java
@@ -1,67 +1,98 @@
 package game;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
 public class Platformer extends JFrame {
-	private static final long serialVersionUID = 5736902251450559962L;
+    private static final long serialVersionUID = 5736902251450559962L;
 
-	BufferedImage levelImg;
+    BufferedImage levelImg;
 
-	public Platformer() {
-		// exit program when window is closed
-		this.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
+    public Platformer() {
+        // exit program when window is closed
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 
-		JFileChooser fc = new JFileChooser();
-		fc.setCurrentDirectory(new File("./"));
-		fc.setDialogTitle("Please input image (.bmp)");
-		FileFilter filter = new FileNameExtensionFilter("image", "bmp");
-		fc.setFileFilter(filter);
-		int result = fc.showOpenDialog(this);
-		File selectedFile = new File("");
+        JFileChooser fc = new JFileChooser();
+        fc.setCurrentDirectory(new File("./"));
+        fc.setDialogTitle("Please input image (.bmp)");
+        FileFilter filter = new FileNameExtensionFilter("image", "bmp");
+        fc.setFileFilter(filter);
+        int result = fc.showOpenDialog(this);
+        File selectedFile = new File("");
 
-		if (result == JFileChooser.APPROVE_OPTION) {
-			selectedFile = fc.getSelectedFile();
-			System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-		} else {
-			dispose();
-			System.exit(0);
-		}
+        if (result == JFileChooser.APPROVE_OPTION) {
+            selectedFile = fc.getSelectedFile();
+            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+        } else {
+            dispose();
+            System.exit(0);
+        }
 
-		try {
-			levelImg = ImageIO.read(selectedFile);
+        try {
+            levelImg = ImageIO.read(selectedFile);
 
-			this.setBounds(0, 0, levelImg.getWidth() + 16, levelImg.getHeight() + 39);
-			this.setSize(1000, 350);
-			this.setLocationRelativeTo(null);
-			this.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            this.setBounds(0, 0, levelImg.getWidth() + 16, levelImg.getHeight() + 39);
+            this.setSize(1000, 350);
+            this.setLocationRelativeTo(null);
+            this.setVisible(true);
+            addKeyAdapter(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
-		new Platformer();
-	}
+    public static void main(String[] args) {
+        new Platformer();
+    }
 
-	@Override
-	public void paint(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.drawImage(levelImg, 8, 31, this);
-	}
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(levelImg, 8, 31, this);
+    }
+
+    public void addKeyAdapter(JFrame frame) {
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                handleKeyPressedEvent(keyEvent);
+            }
+        });
+    }
+
+    public void handleKeyPressedEvent(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.VK_LEFT:
+                handleLeftKeyEvent();
+                break;
+            case KeyEvent.VK_RIGHT:
+                handleRightKeyEvent();
+                break;
+        }
+    }
+
+    private void handleRightKeyEvent() {
+        System.out.println("right arrow ");
+    }
+
+    private void handleLeftKeyEvent() {
+        System.out.println("left arrow ");
+    }
+
+
 }
