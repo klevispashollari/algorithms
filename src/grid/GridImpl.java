205b9e8a0f0c01a7ed60471f31f66d6d6b1e6bbd
Klevis Pashollari <kpashollari@ikub.local>
2020-08-28 11:57:46 +0200

    add unit tests

--- src/grid/GridImpl.java
+++ src/grid/GridImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class GridImpl<E> implements Grid<E> {
 
 	List<List<E>> grid = new ArrayList<>();
-	
+
 	@Override
 	public Iterator<E> iterator() {
 		// TODO Auto-generated method stub
@@ -16,24 +16,36 @@ public class GridImpl<E> implements Grid<E> {
 
 	@Override
 	public void insert(E element, int row, int column) {
-		grid.get(row).set(column, element);
+		if (grid.size() >= row && grid.get(row).size() >= column) {
+			grid.get(row).set(column, element);
+		} else {
+			System.out.println("Index out of bound");
+		}
 	}
 
 	@Override
 	public E remove(int row, int column) {
-		return grid.get(row).remove(column);
+		if (grid.size() >= row && grid.get(row).size() >= column) {
+			return grid.get(row).remove(column);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public E get(int row, int column) {
+		if (grid.size() >= row && grid.get(row).size() >= column) {
 			return grid.get(row).get(column);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
 	public int getMaxColumnSize() {
 		int maxColumnSize = 0;
 		for (List<E> list : grid) {
-			if(list.size() > maxColumnSize) {
+			if (list.size() > maxColumnSize) {
 				maxColumnSize = list.size();
 			}
 		}
@@ -69,6 +81,14 @@ public class GridImpl<E> implements Grid<E> {
 	public Iterator<E> columnIterator() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public List<List<E>> getGrid() {
+		return grid;
+	}
+
+	public void setGrid(List<List<E>> grid) {
+		this.grid = grid;
 	}
 
 }
---
+++ src/grid/GridTestCase.java
@@ -0,0 +1,47 @@
+package grid;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class GridTestCase {
+	GridImpl<String> grid = new GridImpl<String>();
+	
+	
+	public GridImpl<String> getGrid(){
+
+		List<List<String>> gridData = new ArrayList<>();
+		List<String> column = new ArrayList<>();
+		column.add("1");
+		column.add("2");
+		column.add("3");
+		column.add("4");
+		gridData.add(column);
+		gridData.add(column);
+		gridData.add(column);
+		grid.setGrid(gridData);
+		return grid;
+	}
+	
+	@Test
+	void testValidCases() {
+		grid = getGrid();
+		assertEquals("4",grid.get(2, 3));
+		assertEquals("4",grid.remove(2, 3));
+		grid.insert("7", 2, 2);
+		assertEquals("7",grid.get(2, 2));
+	}
+	
+	@Test
+	void testInvalidCases() {
+		grid = getGrid();
+		grid.insert("7", 2, 7);
+		assertEquals(null, grid.get(4,8));
+		assertEquals(null, grid.remove(4,8));
+	}
+}
