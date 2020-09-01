c2f25fa541a301db528a76742c03b6546602e6b4
gledis <gledisgjinaj@gmail.com>
2020-09-01 10:14:50 +0200

    Change import of assertion for CuckooHashMapTest

--- src/grid/CuckooHashMapTest.java
+++ src/grid/CuckooHashMapTest.java
@@ -1,7 +1,7 @@
 package grid;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
