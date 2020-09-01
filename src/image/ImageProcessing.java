bf05eddfede0936eb7a674e9fd2470e7733d6409
Klevis Pashollari <kpashollari@ikub.local>
2020-09-01 09:39:19 +0200

    add basic classes for immage processing

---
+++ src/image/ImageProcessing.java
@@ -0,0 +1,54 @@
+package image;
+
+
+import java.awt.image.BufferedImage;
+
+public class ImageProcessing {
+
+	/**
+	 * Converts the input RGB image to a single-channel gray scale array.
+	 * 
+	 * @param img The input RGB image
+	 * @return A 2-D array with intensities
+	 */
+	private static int[][] convertToGrayScaleArray(BufferedImage img) {
+		
+		// TODO
+		
+		return null;
+	}
+
+	/**
+	 * Converts a single-channel (gray scale) array to an RGB image.
+	 * 
+	 * @param img
+	 * @return BufferedImage
+	 */
+	private static BufferedImage convertToBufferedImage(int[][] img) {
+		
+		// TODO
+		
+		return null;
+	}
+
+	/**
+	 * Converts input image to gray scale and applies the kernel.
+	 * 
+	 * @param img    RGB input image
+	 * @param kernel
+	 * @return convolved gray-scale image
+	 */
+	private static BufferedImage filter(BufferedImage img, Kernel kernel) {
+
+		// TODO
+
+		return null;
+	}
+
+	public static void main(String[] args) {
+
+		// TODO
+
+	}
+
+}
---
+++ src/image/Kernels.java
@@ -0,0 +1,57 @@
+package image;
+
+/**
+ * Holds some kernels which can be convolved with an image.
+ */
+public class Kernels {
+
+	/**
+	 * Multiplies each element in a 2D matrix with a scalar.
+	 * 
+	 * @param m 2D array
+	 * @param v
+	 * @return
+	 */
+	private static double[][] multiplyElementwise(double[][] m, double v) {
+		for (int x = 0; x < m[0].length; x++)
+			for (int y = 0; y < m.length; y++)
+				m[x][y] *= v;
+		return m;
+	}
+
+	public static Kernel GaussianBlur5x5() {
+		double[][] m = { { 1, 4, 6, 4, 1 }, { 4, 16, 24, 16, 4 }, { 6, 24, 36, 24, 6 }, { 4, 16, 24, 16, 4 },
+				{ 1, 4, 6, 4, 1 } };
+		m = multiplyElementwise(m, 1. / 256);
+		return new Kernel(m);
+	}
+
+	public static Kernel BoxBlur3x3() {
+		return new Kernel(
+				new double[][] { { 1. / 9, 1. / 9, 1. / 9 }, { 1. / 9, 1. / 9, 1. / 9 }, { 1. / 9, 1. / 9, 1. / 9 } });
+	}
+
+	public static Kernel EdgeDetection() {
+		return new Kernel(new double[][] { { -1, -1, -1 }, { -1, 8, -1 }, { -1, -1, -1 } });
+	}
+
+	public static Kernel Sharpen() {
+		return new Kernel(new double[][] { { 0, -1, 0 }, { -1, 5, -1 }, { 0, -1, 0 } });
+	}
+
+	public static Kernel Identity() {
+		return new Kernel(new double[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } });
+	}
+
+	public static Kernel Relief() {
+		return new Kernel(new double[][] { { -2, -1, 0 }, { -1, 1, 1 }, { 0, 1, 2 } });
+	}
+
+	public static Kernel MotionBlur() {
+		double[][] m = { { 1, 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 1, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 1, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 1, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 1, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 1 } };
+		m = multiplyElementwise(m, 1. / 9);
+		return new Kernel(m);
+	}
+}
